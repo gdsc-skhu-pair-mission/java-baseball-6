690f07b6990652a69b580bab08cb70b468e6c903
dongkyunKim <kyun010713@gmail.com>
2024-03-30 16:22:43 +0900

    feat(exception) : exception 추가

---
+++ src/main/java/baseball/exception/ErrorMessage.java
@@ -0,0 +1,8 @@
+package baseball.exception;
+
+public class ErrorMessage {
+    public static final String INVALID_LENGTH = "입력은 3자리 숫자여야 합니다.";
+    public static final String NOT_NUMERIC = "입력은 숫자여야 합니다.";
+    public static final String DUPLICATE_DIGITS = "중복된 숫자는 입력할 수 없습니다.";
+}
+
