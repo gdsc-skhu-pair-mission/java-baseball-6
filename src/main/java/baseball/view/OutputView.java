7c92d92c91333736d475ebfe31895187e1e6b535
Yehyeok Bang <qkddpgur318@gmail.com>
2024-04-01 00:46:55 +0900

    feat: 게임 결과 출력 기능 구현

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,27 @@
+package baseball.view;
+
+public class OutputView {
+
+    public void printResultMessage(int strike, int ball) {
+        StringBuilder sb = new StringBuilder();
+
+        if (strike > 0 && ball > 0) {
+            sb.append(ball).append("볼 ").append(strike).append("스트라이크");
+        }
+        if (strike > 0 && ball == 0) {
+            sb.append(strike).append("스트라이크");
+            if (strike > 3) {
+                sb.append("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            }
+        }
+        if (strike == 0 && ball > 0) {
+            sb.append(strike).append("볼");
+        }
+        if (strike == 0 && ball == 0) {
+            sb.append("낫싱");
+        }
+
+        System.out.println(sb);
+    }
+
+}
