02870cf6b8d6bb9089b3c4c95132b6e0615dc47d
bbggr1209 <rsd3010@office.skhu.ac.kr>
2024-04-01 01:06:23 +0900

    메시지출력

---
+++ src/main/java/baseball/view/Output.java
@@ -0,0 +1,22 @@
+package baseball.view;
+
+import static baseball.domain.Massage.*;
+
+public class Output{
+    public static void startMassage() {
+        System.out.println(START);
+    }
+
+    public static void inputMassage() {
+        System.out.println(INPUT);
+    }
+
+    public static void successMassage() {
+        System.out.println(SUCCESS);
+    }
+
+    public static void result() {
+
+    }
+
+}
