3ea2f083462229ae919f083bbc5780febda80c3d
bbggr1209 <rsd3010@office.skhu.ac.kr>
2024-04-01 17:45:10 +0900

    수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,16 +3,8 @@ package baseball;
 import baseball.controller.Game;
 
 public class Application {
-<<<<<<< HEAD
-=======
-        // TODO: 프로그램 구현
->>>>>>> 58c16cfaf53fe709d8d69771a43d71aa76b1fe73
     public static void main(String[] args) {
         Game game = new Game();
         game.gameStart();
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> 58c16cfaf53fe709d8d69771a43d71aa76b1fe73
 }
