4b00a1e04a5592f2c76449a5c620e97a0315448e
kimihwa <ihwag719@gmail.com>
2024-04-02 14:55:19 +0900

    예외 오류

--- src/main/java/baseball/view/Output.java
+++ src/main/java/baseball/view/Output.java
@@ -26,19 +26,6 @@ public class Output{
         int ballCount = result.getBallCount();
         int strikeCount = result.getStrikeCount();
 
-<<<<<<< HEAD
-        ArrayList<String> gameResult = new ArrayList<>();
-        if (ballCount != 0) {
-            gameResult.add(String.format(BALL, ballCount));
-        }
-        if (strikeCount != 0) {
-            gameResult.add(String.format(STRIKE, strikeCount));
-        }
-        if (ballCount == 0 && strikeCount == 0) {
-            gameResult.add(String.format(NOTHING));
-        }
-        System.out.println(String.join(" ",gameResult));
-=======
         ArrayList<String> results = new ArrayList<>();
         if (ballCount != 0)
             results.add(String.format(BALL, ballCount));
@@ -48,7 +35,6 @@ public class Output{
             results.add(String.format(NOTHING));
 
         System.out.println(String.join(" ",results));
->>>>>>> 58c16cfaf53fe709d8d69771a43d71aa76b1fe73
     }
 
 }
